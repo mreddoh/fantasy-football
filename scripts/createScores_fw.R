256dd35fb8b84ca92115a5f397f80ccdf3295fa3
mreddoh <michael.a.hodder@gmail.com>
2021-04-19 10:48:49 +1000

    20210519 weekly update w/ changes to coach name

--- scripts/createScores_fw.R
+++ scripts/createScores_fw.R
@@ -193,4 +193,4 @@ playerStatus <- squadLists %>%
 ### PRINT SCORES
 Scores %>% filter(Round == maxRound) %>% select(-Round) %>% arrange(-Points)
 Ladder %>% arrange(-Points)
-
+Scores %>% filter(Round %in% c(3,4,5,6)) %>% group_by(Coach) %>% summarise(Points = sum(Points)) %>% arrange(-Points)
---
+++ scripts/create_Squadlist.R
@@ -0,0 +1,71 @@
+
+## SET PACKAGES
+library("reshape2")
+library("tidyverse")
+library("lubridate")
+library("fitzRoy")
+library("here")
+library("stringdist")
+library("rvest")
+
+webpage1 <- read_html("https://www.afc.com.au/teams/afl")
+webpage2 <- read_html("https://www.lions.com.au/teams/afl")
+webpage3 <- read_html("https://www.carltonfc.com.au/teams/afl")
+webpage4 <- read_html("https://www.collingwoodfc.com.au/teams/afl")
+webpage5 <- read_html("https://www.essendonfc.com.au/teams/afl")
+webpage6 <- read_html("https://www.fremantlefc.com.au/teams/afl")
+webpage7 <- read_html("https://www.geelongcats.com.au/teams/afl")
+webpage8 <- read_html("https://www.goldcoastfc.com.au/teams/afl")
+webpage9 <- read_html("https://www.gwsgiants.com.au/teams/afl")
+webpage10 <- read_html("https://www.hawthornfc.com.au/teams/afl")
+webpage11 <- read_html("https://www.melbournefc.com.au/teams/afl")
+webpage12 <- read_html("https://www.nmfc.com.au/teams/afl/players")
+webpage13 <- read_html("https://www.portadelaidefc.com.au/teams/afl")
+webpage14 <- read_html("https://www.richmondfc.com.au/football/afl/squad")
+webpage15 <- read_html("https://www.saints.com.au/teams/mens")
+webpage16 <- read_html("https://www.sydneyswans.com.au/teams/afl")
+webpage17 <- read_html("https://www.westcoasteagles.com.au/teams/afl")
+webpage18 <- read_html("https://www.westernbulldogs.com.au/teams/afl")
+
+aflTeams <- c(
+  "Adelaide",
+  "Brisbane Lions",
+  "Carlton",
+  "Collingwood",
+  "Essendon",
+  "Fremantle",
+  "Geelong",
+  "Gold Coast",
+  "GWS",
+  "Hawthorn",
+  "Melbourne",
+  "North Melbourne",
+  "Port Adelaide",
+  "Richmond",
+  "St Kilda",
+  "Sydney",
+  "West Coast",
+  "Footscray"
+)
+
+for (i in 1:18){  
+
+  tempPlayers <- get(paste0("webpage", i)) %>%
+    html_nodes("h1") %>%
+    html_text() %>% 
+    as.data.frame() %>% 
+    rename(Player = names(.[1])) %>% 
+    mutate(cnt = str_count(Player,"\\n"),
+           cnt2 = str_count(Player,"\\t")) %>% 
+    filter(cnt >= 3 & cnt2 == 0) %>% 
+    select(-c(cnt,cnt2)) %>% 
+    mutate(Player = gsub("[\t\n]","",str_squish(Player))) %>% 
+    mutate(Player = trimws(gsub("Key|Midfielder|Forward|Defender|Ruck","",Player))) %>% 
+    mutate(Team = aflTeams[i]) %>% 
+    filter(Player != "AFLW Ladder AFLW Ladder")
+  
+  if(i == 1) { squadLists <- tempPlayers }
+  
+  if(i > 1) { squadLists <- rbind(squadLists, tempPlayers) }
+  
+}
